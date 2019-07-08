ed26572f0d7e1912067ccf5695b48b0a3063f334
senya <kseniiakhromova@yandex.ru>
2019-07-08 08:33:48 +0300

    initial commit

---
+++ TestAutoMapper/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserDTO, User>();
+        }
+    }
+}
---
+++ TestAutoMapper/UserController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TestAutoMapper
+{
+    public class UserController
+    {
+        private readonly IMapper _mapper;
+
+        public UserController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var model = _mapper.Map<UserDTO>(user);
+        }
+    }
+}
